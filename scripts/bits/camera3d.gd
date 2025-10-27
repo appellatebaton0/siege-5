6637e920e821c301ffd097d283416d0191ae7057
appellatebaton0 <145923930+appellatebaton0@users.noreply.github.com>
2025-10-27 15:09:03 -0500

    v0.2 - Still Starting Player (Camera)
    - Barely anything has changed, I got 4 lines into the camera script, just pushing so I can continue work on a different device.

---
+++ scripts/bits/camera3d.gd
@@ -0,0 +1,7 @@
+class_name CameraBit3D extends Bit
+## Provides functionality for a node to pivot along with the mouse.
+
+## The node to pass the Y rotation to
+@export var y_target:NodeValue
+## The node to pass the X rotation to
+@export var x_target:NodeValue
