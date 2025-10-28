bce6f924e985e00f5f9cc410b95603462c2ac29c
appellatebaton0 <145923930+appellatebaton0@users.noreply.github.com>
2025-10-28 10:59:46 -0500

    v0.3 - Camera
    - The camera works, though it definitely needs sensitivity tweaks, and I'll need to set up the UI before I make it capture the mouse.

--- scripts/bits/camera3d.gd
+++ scripts/bits/camera3d.gd
@@ -5,3 +5,18 @@ class_name CameraBit3D extends Bit
 @export var y_target:NodeValue
 ## The node to pass the X rotation to
 @export var x_target:NodeValue
+
+@export var sensitivity:float = 5
+
+func set_valid(value:NodeValue, amount:Vector3):
+	if value != null:
+		var node = value.value()
+		if node is Node3D:
+			node.rotation += amount
+
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		set_valid(x_target, Vector3(event.relative.y * 0.001 * sensitivity, 0, 0))
+		set_valid(y_target, Vector3(0, event.relative.x * 0.001 * sensitivity, 0))
+		
